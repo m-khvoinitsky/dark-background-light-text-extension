7df73fee1e08152cf25705f06424cc704f283611
Mikhail Khvoinitsky <me@khvoinitsky.org>
2016-02-04 16:33:40 +0300

    improved url matching

--- test/test-index.js
+++ test/test-index.js
@@ -20,14 +20,18 @@ require("sdk/test").run(exports);
 */
 
 const main = require('../lib/index');
-exports['test urls'] = () => {
+exports['test urls'] = assert => {
+    assert.ok(true);
     [
         'about:config',
         'https://www.bla.www.co.uk.shit.kokoko.website.co.uk/en-US/products/firefox?as=u&utm_source=inproduct#asdfasdf=qwer',
-        'https://dick.corp.wargaming.local/en-US/products/firefox?as=u&utm_source=inproduct#asdfasdf=qwer',
+        'https://dick.corp.company.local/en-US/products/firefox?as=u&utm_source=inproduct#asdfasdf=qwer',
         'https://dick/en-US/products/firefox?as=u&utm_source=inproduct#asdfasdf=qwer',
         'https://support.mozilla.org/en-US/products/firefox?as=u&utm_source=inproduct#asdfasdf=qwer',
         'resource:///chrome/browser/content/branding/aboutDialog.css',
-        'file:///home/misha/dark-background-light-text/test/test-pages/target-blank/1st.html'
+        'file:///home/misha/dark-background-light-text/test/test-pages/target-blank/1st.html',
+        'resource://jid1-qofqdk4qzufgwq-at-jetpack/data/preferences.html',
+        'chrome://browser/content/browser.xul',
+        'chrome://browser/content/browser_.xul'
     ].forEach(path => console.log(path, main.generate_urls(path)));
 };
