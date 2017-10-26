4c4fe6ae5596d42558b6e06aae3893b5bddd74ac
Mikhail Khvoinitsky <me@khvoinitsky.org>
2017-10-26 12:30:44 +0300

    bug 1393022 / #96 workaround: initial implementation, resolve relative urls in background-image to absolute

--- methods/stylesheet-processor-abstract.js
+++ methods/stylesheet-processor-abstract.js
@@ -29,6 +29,102 @@ const brackets_aware_split = (value, separator) => {
 const inline_override_stylesheet_id = 'dark-background-light-text-add-on-inline-style-override';
 const quote_re = new RegExp('"', 'g');
 const important_re = new RegExp('!important;', 'g');
+const import_rule_re = new RegExp('@import.*;', 'g');
+
+//TODO: rewrite it!!!!!
+const getPath = str => /(?:url\()(?:.*?)(?:\))|(["'])(?:[^"')]+)\1/ig.exec(str)[0]
+	.replace(/(?:url\()/ig, '')
+	.replace(/(?:\))/g, '')
+	.replace(/(?:["'])/g, '')
+    .trim();
+
+const at_import = '@import';
+function parse_quotedstring(s) {
+
+}
+function parse_at_import_rule(at_import_rule) {
+    at_import_rule = at_import_rule.trim();
+    // remove leading '@import' and trailing ';'
+    at_import_rule = at_import_rule.substring(
+        at_import_rule.indexOf(at_import) === 0 ? at_import.length : 0,
+        at_import_rule.indexOf(';') + 1 === at_import_rule.length ? at_import_rule.length - 1 : at_import_rule.length
+    ).trim();
+
+    let url;
+    let media_queries_str;
+
+    if (at_import_rule.indexOf('url(') === 0) {
+        let splitted = brackets_aware_split(at_import_rule, ' ');
+        url = splitted.shift();
+        url = url.substring(4, url.length - 1);
+        media_queries_str = splitted.join(' ');
+    }
+
+    if (at_import_rule[0] === '"' || at_import_rule[0] === "'") {
+        let splitted = brackets_aware_split(at_import_rule, ' ');
+        url = splitted.shift();
+        media_queries_str = splitted.join(' ');
+    } else if (at_import_rule[0] === '"' || at_import_rule[0] === "'") {
+
+    } else {
+        let splitted = at_import_rule.split(' ');
+        url = splitted.shift();
+        media_queries_str = splitted.join(' ');
+    }
+
+    let in_brackets = false;
+    let in_singlequotes = false;
+    let in_doublequotes = false;
+    let escape_char = false;
+    let url = '';
+    for (let c of at_import_rule) {
+        if (in_singlequotes || in_doublequotes) {
+            if (escape_char) {
+                url += c;
+                escape_char = false;
+            } else if (c === '\\')
+                escape_char = true;
+            else if (in_singlequotes && c === "'")
+                in_singlequotes = false;
+            else if (in_doublequotes && c === '"')
+                in_doublequotes = false;
+            else
+                url += c;
+        } else if (c === '"')
+            in_doublequotes = true;
+        else if (c === "'")
+            in_singlequotes = true;
+        else
+            url += c;
+    }
+    console.log(`url is: ${url}`);
+}
+
+async function fetch_and_insert_stylesheet(url, rel_to, media) {
+    // Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1393022
+    // TODO: resolve relative urls for CSS properties other than background(-image) (cursor, @font-face, list-style etc.)
+
+    let url_obj = new URL(url, rel_to);
+    let css = await (await fetch(url_obj)).text();
+    let import_rules = css.match(import_rule_re);
+    if (import_rules) {
+        for (let rule_str of import_rules) {
+            //TODO: use correct media rules
+            parse_at_import_rule(rule_str);
+            fetch_and_insert_stylesheet(
+                getPath(rule_str),
+                url_obj
+            ).catch(error => console.error(error));
+        }
+        css = css.replace(import_rule_re, '');
+    }
+
+    let style = document.createElement('style');
+    style.innerText = css;
+    style.setAttribute('data-original-href', url_obj.href);
+    document.head.appendChild(style);
+    console.log('Worked around');
+}
 
 class StylesheetProcessorAbstract {
     constructor(window, options, style_selector) {
@@ -141,6 +237,8 @@ class StylesheetProcessorAbstract {
             if (!this.processed_stylesheets.has(sheet) && sheet !== this.inline_override) {
                 if (this.process_CSSStyleSheet(sheet))
                     this.processed_stylesheets.set(sheet, sheet.cssRules.length);
+                else
+                    console.log(["fail!!!!", sheet]);
             }
         });
         Array.prototype.forEach.call(
@@ -159,46 +257,59 @@ class StylesheetProcessorAbstract {
         if (!(this.window.document.hidden) || (this.window.document.readyState !== 'complete'))
             setTimeout(() => this.process(), this.window.document.readyState !== 'complete' ? 100 : 1000);
     }
-    process_CSSStyleSheet(CSSStyleSheet_v) {
+    process_CSSStyleSheet(CSSStyleSheet_v, base_url) {
         if (this.stop)
             return false;
         try {
-            CSSStyleSheet_v.cssRules;
+            if (CSSStyleSheet_v.cssRules === null) // access to .cssRules will throw in Firefox
+                throw {name: 'SecurityError'} // for chrome
         } catch (e) {
             // weird shit, but it happens
             // it seems that such stylesheet just not yet ready
             // TODO: will be great to know it exactly
+            console.error(e);
+            console.log(CSSStyleSheet_v);
+            if (e.name === 'SecurityError') {
+                // TODO: !!!!!!! do something about
+                CSSStyleSheet_v.ownerNode.parentNode.removeChild(CSSStyleSheet_v.ownerNode);
+                fetch_and_insert_stylesheet(
+                    CSSStyleSheet_v.href,
+                    document.documentURI
+                ).catch(error => console.error(error));
+            }
             return false;
         }
+        if (!base_url) {
+            if (CSSStyleSheet_v.href && CSSStyleSheet_v.href.indexOf('data:') !== 0)
+                base_url = new URL(CSSStyleSheet_v.href, document.documentURI).href;
+            else if (CSSStyleSheet_v.ownerNode && CSSStyleSheet_v.ownerNode.hasAttribute('data-original-href'))
+                base_url = CSSStyleSheet_v.ownerNode.getAttribute('data-original-href');
+            else
+                base_url = document.documentURI;
+        }
         Array.prototype.forEach.call(CSSStyleSheet_v.cssRules, rule => {
-            this.process_CSSRule(rule);
+            this.process_CSSRule(rule, base_url);
         });
         return true;
     }
-    process_CSSRule(CSSRule_v) {
+    process_CSSRule(CSSRule_v, base_url) {
         if (this.stop)
             return;
         switch (CSSRule_v.type) {
             case 1: // CSSRule.STYLE_RULE
-                this.process_CSSStyleRule(CSSRule_v);
+                this.process_CSSStyleRule(CSSRule_v, base_url);
                 break;
             case 3: // CSSRule.IMPORT_RULE
                 //this.process_CSSImportRule(CSSRule_v);
-                this.process_CSSStyleSheet(CSSRule_v.styleSheet);
+                this.process_CSSStyleSheet(CSSRule_v.styleSheet, base_url);
                 break;
             case 4: // CSSRule.MEDIA_RULE
                 //this.process_CSSMediaRule(CSSRule_v);
-                this.process_CSSStyleSheet(CSSRule_v);
+                this.process_CSSStyleSheet(CSSRule_v, base_url);
                 break;
         }
     }
-    process_CSSStyleRule(CSSStyleRule_v) {
-        let base_url;
-        try {
-            base_url = CSSStyleRule_v.parentStyleSheet.href;
-        } catch (e) {}
-        if (!base_url) //CSSStyleRule_v.parentStyleSheet.ownerNode is HTMLStyleElement, so, it has no href
-            base_url = this.window.location.href;
+    process_CSSStyleRule(CSSStyleRule_v, base_url) {
         let selector = CSSStyleRule_v.selectorText;
         this.process_CSSStyleDeclaration(
             CSSStyleRule_v.style, // CSSStyleDeclaration
