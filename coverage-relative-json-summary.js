3fdcd87cd8f3e83c1e7151b50ecad4388a2cb0ec
Mikhail Khvoinitsky <me@khvoinitsky.org>
2021-01-09 02:05:04 +0300

    coverage tracking, do not allow it to decrease on per-file basis

---
+++ coverage-relative-json-summary.js
@@ -0,0 +1,30 @@
+'use strict';
+const { ReportBase } = require('istanbul-lib-report');
+const { writeFileSync } = require('fs');
+const { relative } = require('path');
+
+class RelativeJsonSummaryReport extends ReportBase {
+  constructor() {
+    super();
+
+    this.data = {};
+  }
+
+  onDetail(node) {
+    this.data[
+      relative(
+        process.cwd(),
+        node.getFileCoverage().path,
+      )
+    ] = node.getCoverageSummary();
+  }
+
+  onEnd() {
+    writeFileSync(
+      'last-coverage-summary.json',
+      `${JSON.stringify(this.data, null, 4)}\n`,
+      {encoding: 'utf8'},
+    );
+  }
+}
+module.exports = RelativeJsonSummaryReport;
